003eaa28acab4e80a83c9f6273c8e10ec0e08b52
Kolya Venturi <me@kolya.co>
2021-09-03 09:06:16 -0700

    Initial pass on categorizing some effects

---
+++ src/constants/issues.ts
@@ -0,0 +1,10 @@
+export const defaultTitles = [
+  '[Effect Suggestion] My Super Cool Effect Idea',
+  '[Feature Suggestion] My Neat Feature',
+  '[Bug] My Nasty Bug'
+];
+
+export const overUsedEffects = [
+  'cougar',
+  'cougars'
+];
