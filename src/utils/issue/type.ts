2bf9dea982b5f86e9064c91c04fde1abe1293311
Kolya Venturi <me@kolya.co>
2021-09-04 11:25:57 -0700

    Remove comment

--- src/utils/issue/type.ts
+++ src/utils/issue/type.ts
@@ -1,8 +1,5 @@
 import {IssueType, Label} from './types';
 
-// TODO: Add method that can take the list of issue tags, and return the proper enum type
-// the return likely needs to be IssueType | null, in the event that we have a missing tag?
-// That probably needs to be later handled in the problems.ts file
 const labelMap: Record<string, IssueType> = {
   'effect suggestion': IssueType.EFFECT,
   'feature request': IssueType.FEATURE,
